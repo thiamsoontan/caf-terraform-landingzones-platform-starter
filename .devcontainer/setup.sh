bf75ff49b025ed5bd6099679a56de88899fd3f15
thiamsoontan <thiamsoontan@microsoft.com>
2023-06-07 15:13:12 +0800

    update devcontainer and patches landing zones

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -23,6 +23,21 @@ git config --global --add safe.directory /tf/caf/aztfmod
 git config pull.rebase false 
 
 if [ ! -d /tf/caf/landingzones ]; then
-  git clone --branch int-5.6.0 https://github.com/Azure/caf-terraform-landingzones.git /tf/caf/landingzones
+  # git clone --branch int-5.6.0 https://github.com/Azure/caf-terraform-landingzones.git /tf/caf/landingzones
+  # clone latest caf terraform landingzones - 5.69
+  git clone https://github.com/Azure/caf-terraform-landingzones.git /tf/caf/landingzones
   sudo chmod +x /tf/caf/landingzones/templates/**/*.sh
+  # git clone aztfmod (if required)
+  if [ ! -d /tf/caf/landingzones/aztfmod ]; then
+    # clone latest version of aztfmod
+    git clone https://github.com/aztfmod/terraform-azurerm-caf.git /tf/caf/landingzones/aztfmod 
+    cd /tf/caf/landingzones/aztfmod 
+    # checkout version 5.7.0-preview0
+    git checkout 5.7.0-preview0
+    # patch to use local copy of aztfmod
+    cp /tf/caf/patches/caf_launchpad/landingzone.tf /tf/caf/landingzones/caf_launchpad/landingzone.tf
+    cp /tf/caf/patches/caf_solution/landingzone.tf /tf/caf/landingzones/caf_solution/landingzone.tf
+    cd /tf/caf/
+    # note: /tf/caf/landingzones/aztfmod/main.tf show the version of the azurerm
+  fi
 fi
